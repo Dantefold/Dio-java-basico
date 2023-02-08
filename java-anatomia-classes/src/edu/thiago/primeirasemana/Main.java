3a9f85736237f816c4189f986aaf8e62878b6eba
Dantefold <nerofold@gmail.com>
2023-02-08 00:48:25 -0300

    backup

---
+++ java-anatomia-classes/src/edu/thiago/primeirasemana/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo");
+        System.out.println("Sou eu de novo");
+        System.out.println("to ficando fera");
+    }
+}
+
+// para comentar em uma linha
+/* para comentar em varias linhas*/ 
